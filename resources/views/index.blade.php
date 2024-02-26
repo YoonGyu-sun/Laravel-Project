441ee269d1a49ea678af1e885053c91f3d448109
YoonGyu-Sun <dbsrbtjs@naver.com>
2024-02-26 11:29:11 +0900

    2th

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    
+    @vite(['resources/css/index.css','resources/js/script.js'])
 </head>
 <body>
     <h1>Hello world</h1>
 
+
     
-    @vite(['resources/css/index.css','resources/js/script.js'])
+    
 </body>
 </html>
